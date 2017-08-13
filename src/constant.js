eb21529448a58ee81f4206099a2389ad5f26f843
mcd-50 <ayush.as.shukla@gmail.com>
2017-08-13 06:02:52 +0530

    First commit

---
+++ src/constant.js
@@ -0,0 +1,10 @@
+export const SERVER_URL = 'https://finper.eu-gb.mybluemix.net/api/response'
+
+
+export const CONNECTION = 'connection';
+export const MESSAGE = 'message';
+export const JOIN = 'join';
+export const LEAVE = 'leave';
+export const JOINED = 'joined';
+export const LEFT = 'left';
+export const ERROR = 'error';
